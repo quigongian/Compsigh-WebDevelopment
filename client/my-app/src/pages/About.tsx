4279a10a22ef1284e5c88e5a9422584101a4d001
Carlos Chavez <cenric09@gmail.com>
2022-10-27 22:15:43 -0400

    about page template done

--- client/my-app/src/pages/About.tsx
+++ client/my-app/src/pages/About.tsx
@@ -2,14 +2,43 @@ import React from "react";
 import { Subheading, Paragraph } from "../globalStyles";
 import "./About.css";
 
+type blockProps = {
+  heading: string;
+  description: string;
+};
+
 export const About = () => {
   return (
     <div className="wrapper">
-      <Block />
+      <div className="row">
+        <Block
+          heading="Who We Are"
+          description="Lorem Ipsum dfjdnfjdn vfkvmkm vkfmvfi  vifmvfim vifmvf i fimvfivm vfm imf fimfv"
+        />
+        <Block
+          heading="What We Can Do For You"
+          description="Lorem Ipsum dfjdnfjdn vfkvmkm vkfmvfi  vifmvfim vifmvf i fimvfivm vfm imf fimfv"
+        />
+      </div>
+      <div className="row">
+        <Block
+          heading="Our Story"
+          description="Lorem Ipsum dfjdnfjdn vfkvmkm vkfmvfi  vifmvfim vifmvf i fimvfivm vfm imf fimfv"
+        />
+        <Block
+          heading="Something Else Here"
+          description="Lorem Ipsum dfjdnfjdn vfkvmkm vkfmvfi  vifmvfim vifmvf i fimvfivm vfm imf fimfv"
+        />
+      </div>
     </div>
   );
 };
 
-const Block = () => {
-  return <div className=""></div>;
+const Block = ({ heading, description }: blockProps) => {
+  return (
+    <div className="block">
+      <Subheading>{heading}</Subheading>
+      <Paragraph className="p">{description}</Paragraph>
+    </div>
+  );
 };
