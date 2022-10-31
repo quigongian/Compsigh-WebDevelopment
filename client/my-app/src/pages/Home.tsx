2df5714456fa0da57a514c84067daa565b636ad0
Laura <lauradavalos0170@gmail.com>
2022-10-30 23:45:38 -0400

    finished the buttons and format

--- client/my-app/src/pages/Home.tsx
+++ client/my-app/src/pages/Home.tsx
@@ -1,6 +1,4 @@
 import React, { useState } from "react"
-import topWave from "../image_content/topWave.svg";
-import bottomWave from "../image_content/bottomWave.svg";
 import { Header } from "../components/header"
 import "./home.css";
 
@@ -29,17 +27,21 @@ export const Home = () => {
             {clickedButton === "btn1"
               ? `blah blach blahcahca`
               : ""}
-               {clickedButton === "btn2"
+            {clickedButton === "btn2"
               ? `blah `
               : ""}
-               {clickedButton === "btn3"
+            {clickedButton === "btn3"
               ? `blah bleh`
               : ""}
-               {clickedButton === "btn4"
+            {clickedButton === "btn4"
               ? `blah bloo`
               : ""}
           </p>
-
+          {/* 'CMD + /' = comments
+        <div className="Imgs">
+        <img className="top wave" src={topWave} alt="" width={"100%"} />
+        </div>
+*/}
           <button className="main-btn">Get Started</button>
           <div className="btn-group">
             <button onClick={buttonHandler} className="button" name="btn1"></button>
@@ -48,6 +50,7 @@ export const Home = () => {
             <button onClick={buttonHandler} className="button" name="btn4"></button>
           </div>
         </div>
+
         <div className="Sect2">
           <h2>One less thing to worry about</h2>
         </div>
