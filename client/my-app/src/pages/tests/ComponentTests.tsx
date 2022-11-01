f78ed4515095c2312aca1f6e059594b8c7d16ca6
tehniel <skydan.inc@gmail.com>
2022-11-01 17:40:51 -0400

    dash blob

--- client/my-app/src/pages/tests/ComponentTests.tsx
+++ client/my-app/src/pages/tests/ComponentTests.tsx
@@ -1,4 +1,9 @@
 import React from "react";
+import { TimerPage } from "../dashboard/Timer";
 export const ComponentTests = () => {
-  return <h1>Component Tests works</h1>;
+  return (
+    <>
+    <TimerPage/>
+    </>
+  )
 };
