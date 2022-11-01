116df896866b56b258b385930359278cc8b9be4a
Eva Sennrich <evasennrich@gmail.com>
2022-11-01 18:05:09 -0400

    deleted dashboard folder

--- client/my-app/src/pages/dashboard/CheckIn.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react"
-export const CheckInPage = () => {
-    return (
-        <h1>CheckIn works</h1>
-    );}
--- client/my-app/src/pages/dashboard/landing.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react"
-export const LandingPage = () => {
-    return (
-        <h1>LandingDash works</h1>
-    );}
