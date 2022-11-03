dc751e6727bc01ec83819a31ab341c7082b84af7
Eva Sennrich <evasennrich@gmail.com>
2022-11-03 17:36:58 -0400

    sidebar rendering conditions and styling done

--- client/my-app/src/components/dashboard_components/timer.tsx
+++ client/my-app/src/components/dashboard_components/timer.tsx
@@ -2,9 +2,9 @@ import React from "react";
 
 export const Timer = () => {
   return (
-    <>
+    <div style={{ marginLeft: "15%" }}>
       <h1>This is just to prevent errors. Components may need to be setup differently.</h1>
       <h1>Remember components are being rendered in pages</h1>
-    </>
+    </div>
   );
 };
--- client/my-app/src/components/dashboard_components/to-do.tsx
+++ client/my-app/src/components/dashboard_components/to-do.tsx
@@ -1,69 +1,60 @@
-import React from "react"
+import React from "react";
 
+const Container = {
+  border: "1px solid red",
+};
+const Wrapper = {
+  display: "flex",
+  height: "90vh",
+  width: "100%",
+};
+const Sidebar = {
+  border: "2px solid black",
+  height: "100%",
+  width: "125px",
+};
 
-const Container={
-    border: "1px solid red",
+const rightContainer = {
+  border: "1px solid blue",
+  height: "100%",
+  width: "100%",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+};
 
-    
-}
-const Wrapper={
-    display: "flex",
-    height: "90vh",
-    width: "100%",  
-}
-const Sidebar={
-    border: "2px solid black",
-    height: "100%",
-    width: "125px",    
-}
+const toDoList = {
+  border: "2px solid purple",
+  height: "92%",
+  width: "90%",
+  margin: "20px",
+  justifyContent: "center",
+};
 
-const rightContainer={
-    border: "1px solid blue",
-    height: "100%",
-    width: "100%",
-    display: "flex",
-    alignItems: 'center',
-    justifyContent: 'center',
-    
-}
-
-const toDoList={
-    border: "2px solid purple",
-    height: "92%",
-    width: "90%",
-    margin: "20px",
-    justifyContent: 'center',
-    
-}
-
-const textBox={
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: "70%",
-    border: "1px solid green",
-    display: "flex"
-}
-
+const textBox = {
+  alignItems: "center",
+  justifyContent: "center",
+  width: "70%",
+  border: "1px solid green",
+  display: "flex",
+};
 
 export const Todo = () => {
-    return (
-        <>
-        <div style ={Container}>
-            <div style ={Wrapper}>
-            <div style={Sidebar}></div>
-                <div style ={rightContainer}>
-
-                <div style={toDoList}>
-                    <div style ={textBox}>
-                        <p>To-Do List</p>
-                        <p>Filter</p>
-                    </div>
-                </div>
-
-                </div>
+  return (
+    <>
+      <div style={Container}>
+        <div style={Wrapper}>
+          <div style={Sidebar}></div>
+          <div style={rightContainer}>
+            <div style={toDoList}>
+              <div style={textBox}>
+                <p>To-Do List</p>
+                <p>Filter</p>
+              </div>
             </div>
+          </div>
         </div>
-        
-        </>
-    );
-}
+      </div>
+    </>
+  );
+};
