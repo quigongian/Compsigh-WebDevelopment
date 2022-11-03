3b4215194ab83a815a3938ed48c21012e6544639
tehniel <skydan.inc@gmail.com>
2022-11-03 16:56:05 -0400

    add timer path

--- client/my-app/src/components/dashboard_components/timer.tsx
+++ client/my-app/src/components/dashboard_components/timer.tsx
@@ -1,10 +1,14 @@
 import React from "react";
+import './timer.css'
+
 
 export const Timer = () => {
   return (
     <>
+    <div className="timer-wrapper">
       <h1>This is just to prevent errors. Components may need to be setup differently.</h1>
       <h1>Remember components are being rendered in pages</h1>
+    </div>
     </>
   );
 };
