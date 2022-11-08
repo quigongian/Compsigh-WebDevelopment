d14e5981a6cbac5eb0cc69c38d0797d64089230e
Eva Sennrich <evasennrich@gmail.com>
2022-11-08 18:24:31 -0500

    css quotes generator bg done

--- client/my-app/src/components/dashboard_components/landing.tsx
+++ client/my-app/src/components/dashboard_components/landing.tsx
@@ -1,9 +1,21 @@
 import React from "react";
+import quotesWaves from "../../image_content/quotesWaves.svg";
+import "./landing.css";
+
 export const Landing = () => {
   return (
-    <div style={{ marginLeft: "15%" }}>
-      <h1>This is just to prevent errors. Components may need to be setup differently.</h1>
-      <h1>Remember components are being rendered in pages</h1>
+    <div style={{ marginLeft: "25%" }}>
+      <Quotes />
+    </div>
+  );
+};
+
+const Quotes = () => {
+  return (
+    <div className="quotes">
+      <div className="bigQuotentionMarks">{"HELLOOOOOOOO"}</div>
+      <div className="rndQuotes"></div>
+      <img className="quotesWaves" src={quotesWaves} alt="quotesBGx" />
     </div>
   );
 };
