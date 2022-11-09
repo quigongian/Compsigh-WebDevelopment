3468ec68ba0fa3276b567c3eaf349a6bffa3d9c5
[LauraAurora] <[laura.apenza@gmail.com]>
2022-11-09 13:39:38 -0500

    Created the container for questions

--- client/my-app/src/components/dashboard_components/check-in.tsx
+++ client/my-app/src/components/dashboard_components/check-in.tsx
@@ -1,9 +1,45 @@
 import React from "react";
+import bottomWave from "../image_content/bottomWave.svg";
+import midWave from "../image_content/midWave.svg";
+import secondWave from "../image_content/secondWave.svg";
+import topWave from "../image_content/topWave.svg";
+import "./check-in.css";
+
 export const CheckIn = () => {
   return (
-    <div style={{ marginLeft: "15%" }}>
-      <h1>This is just to prevent errors. Components may need to be setup differently.</h1>
-      <h1>Remember components are being rendered in pages</h1>
+    <>
+    <div style={{marginLeft: "35%", display: "grid"}}>
+        {/* <h1>Hey Doge, how was your day?</h1>
+        <h3>November 9, 2022</h3> */}
+        <Questions />
+        <Calendar />
+        <Previous />
+    </div>
+    </>
+  );
+};
+
+
+const Questions = () => {
+  return (
+  <div className = "questions">
+
+  </div>
+  );
+};
+
+const Calendar = () => {
+  return (
+    <div className = "calendar">
+
+    </div>
+  );
+};
+
+const Previous = () => {
+  return (
+    <div className = "previous">
+
     </div>
   );
 };
---
+++ client/my-app/src/pages/dashboard/CheckIn.tsx
@@ -0,0 +1,5 @@
+import React from "react"
+export const CheckInPage = () => {
+    return (
+        <h1>CheckIn works</h1>
+    );}
