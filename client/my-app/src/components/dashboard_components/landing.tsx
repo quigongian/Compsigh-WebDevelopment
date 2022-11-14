d0937cdaf67299390e678e4fc62f82ff979903e5
Eva Sennrich <evasennrich@gmail.com>
2022-11-13 23:59:28 -0500

    added json file for randomizing quotes

--- client/my-app/src/components/dashboard_components/landing.tsx
+++ client/my-app/src/components/dashboard_components/landing.tsx
@@ -1,6 +1,7 @@
 import quotationMarks from "../../image_content/quotationMarks.svg";
 import quotesBG from "../../image_content/quotesBG.svg";
 import "./landing.css";
+import quotesJSON from "./quotes.json";
 
 export const Landing = () => {
   return (
@@ -11,15 +12,14 @@ export const Landing = () => {
 };
 
 export const Quotes = () => {
+  const num = Math.floor(Math.random() * quotesJSON.quotes.length);
   return (
     <div className="quotes">
       <img className="quotationMarks" src={quotationMarks} alt="quotationMarks" />
       <img className="quotesBG" src={quotesBG} alt="quotationMarks" />
-      <div className="rndQuotes">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        <div className="creditsContainer">
-          <span className="quotesCredits">-Lorem Ipsum</span>
-        </div>
+      <div className="rndQuotes">{quotesJSON.quotes[num].quote}</div>
+      <div className="creditsContainer">
+        <div className="quotesCredits">- {quotesJSON.quotes[num].author}</div>
       </div>
     </div>
   );
