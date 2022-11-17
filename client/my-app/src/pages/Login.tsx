7b633b028d4c29903c66ba376cedc3e09e987458
snhs1106 <stephanieh0611@gmail.com>
2022-11-16 21:04:36 -0500

    Updated Login

--- client/my-app/src/pages/Login.tsx
+++ client/my-app/src/pages/Login.tsx
@@ -3,8 +3,9 @@ import { CenterFocusStrong } from "@mui/icons-material";
 import { useState } from "react";
 import { Footer } from "../components/footer";
 import { Header } from "../components/header";
-import  NewSignIn from "../image_content/newSignIn.png";
-import  SignIn from "../image_content/signIn.png";
+// import  NewSignIn from "../image_content/newSignIn.png";
+// sign in
+import  SignOut from "../image_content/signOut.png";
 import "./Login.css";
 
 
@@ -16,61 +17,64 @@ export const Login = () => {
     <>
       <Header />
       {display === "signIn" && (
-        <div style={{}}>
+
         <section style={{ display: "flex", flexDirection: "row", justifyContent: "space-between"}} >
-          <section style={{margin: "auto", marginTop: "100px", textAlign: "center"}}>
-            <h1 style={{fontSize: "50px", marginBottom: "80px"}}>Welcome Back!</h1>
+          <section className="section">
+            <h1>Welcome Back!</h1>
             <form action="">
           
               <div>
-                <input type="email" name="email" placeholder="E-Mail *" required style={{fontSize: "18px", backgroundColor: "#D9D9D9", marginBottom: "30px", borderRadius: "8px", border: "none", padding: "11px 210px 11px 25px"}}/>
+                <input className="input" type="email" name="email" placeholder="E-Mail *" required id="email"/>
               </div>
 
               <div>
-                <input type="password" name="password" placeholder="Password *" required style={{fontSize: "18px", backgroundColor: "#D9D9D9", marginBottom: "13px", borderRadius: "8px", border: "none", padding: "11px 210px 11px 25px"}}/>
+                <input className="input" type="password" name="password" placeholder="Password *" required id="password"/>
               </div>
             
-            <span style={{ display: "flex", flexDirection: "row", justifyContent: "space-between"}}>
-              <div>
-              <input type="checkbox" name="rememberMe" id="rememberMe"/>
-              <label htmlFor="rememberMe">Remember me</label>
-              </div>
-              <div></div>
-              <button onClick={() => setDisplay("forgotPassword")} style={{fontSize: "16px", textDecoration: "underline", border: "none", fontFamily: "-apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif"}}>Forgot password?
-              </button>
-            </span>
-              <div style={{marginTop: "50px"}}>
-                <button style={{borderRadius: "12px", border: "none", backgroundColor: "#81B29A", padding: "5px 15px", fontSize: "18px", color: "white", fontWeight: "bold"}}>Sign in</button>
+              <span>
+              
+                <div>
+                <input type="checkbox" name="rememberMe" id="rememberMe"/>
+                <label htmlFor="rememberMe">Remember me</label>
+                </div>
+
+                <button className="forgot" onClick={() => setDisplay("forgotPassword")}>Forgot password?</button>
+                
+              </span>
+
+              <div style={{marginTop: "57px"}}>
+                <button className="signIn">Sign in</button>
               </div>
 
-              <div>
-                <h4 onClick={() => setDisplay("signUp")} style={{ textDecoration: "underline" }}>
-                  Don't have an account? Sign Up
-                </h4>
-              </div>
+              <button className="account" onClick={() => setDisplay("signUp")}>Don't have an account? Sign Up</button>
+
             </form>
           </section>
-          <section style={{height: "920px"}}>
-            <img src={NewSignIn} alt="" style= {{width: "700px", height: "920px"}} />
+
+          <section style={{height: "740px"}}>
+            <img src={SignOut} alt=""/>
           </section>
+
         </section>
-        </div>
       )}
+
       {display === "signUp" && (
+
         <section style={{ display: "flex", flexDirection: "row", justifyContent: "space-between" }}>
-        <section style={{ marginLeft: "100px" }}>
-          <h1 style={{}}>Welcome!</h1>
+        <section className="section" id="section2">
+          <h1 id="welcome">Welcome!</h1>
           <form action="">
-            <input type="text" name="firstname" placeholder="First Name" required />
+
+            <input className="input" id="input" type="text" name="firstname" placeholder="First Name *" required />
             <br />
-            <input type="text" name="lastname" placeholder="Last Name" required />
+            <input className="input" id="input" type="text" name="lastname" placeholder="Last Name *" required />
             <br />
-            <input type="email" name="email" placeholder="E-Mail" required />
+            <input className="input" id="input" type="email" name="email" placeholder="E-Mail *" required />
             <br />
-            <input type="password" name="password" placeholder="Password" required />
+            <input className="input" id="input" type="password" name="password" placeholder="Password *" required />
             <br />
 
-            <select name="experiencelevel" id="explevel">
+            <select className="dropDown" name="experienceLevel" id="expLevel">
               <option value="">Experience Level</option>
               <option value="">Beginner</option>
               <option value="">Intermediate</option>
@@ -79,64 +83,58 @@ export const Login = () => {
             <br />
 
             {/* how do i make this required and have a placeholde */}
-            <select name="careerpath" id="careerpath">
-              <option value="careerpath">Select a Career Path</option>
+            {/* how do i change the padding on the arrow */}
+            <select className="dropDown" name="careerPath" id="careerPath">
+              <option value="careerPath">Select a Career Path</option>
               <option value="cs">Computer Science</option>
               <option value="ch">Computer Hardware</option>
               <option value="cyber">Cyber Security</option>
-              <option value="webdev">Web Development</option>
+              <option value="webDev">Web Development</option>
               <option value="it">Information Technology</option>
               <option value="swe">Software Developer</option>
               <option value="uiux">UI/UX Design</option>
             </select>
             <br />
 
-            <input type="checkbox" id="t&c" name="terms&conditions" />
-            <label htmlFor="t&c" id="t&c">
-              I read and agree to the{" "}
-              {/* <h4 > 
-                Terms & Conditions
-              </h4>
-                <a href="terms&conditions.html">Terms & Conditions</a> change */}
-            </label>
+            <button className="signIn">Sign Up</button>
             <br />
 
-            <button>Sign Up</button>
-            <br />
-            <h4 onClick={() => setDisplay("signIn")} style={{ textDecoration: "underline" }}>
-              Already have an account? Sign in
-            </h4>
+            <button className="account"onClick={() => setDisplay("signIn")}>Already have an account? Sign in</button>
+            
           </form>
         </section>
-        <section style={{height:"920px"}}>
-        <img src={SignIn} alt="" style= {{width: "600px", height: "920px"}}/>
-      </section>
+
+        <section style={{height:"740px"}}>
+          <img src={SignOut} alt=""/>
+        </section>
+
       </section>
       )}
 
-      {/* make this a pop up screen with an 'x' to close it  */}
       {display === "forgotPassword" && (
         <section style={{ display: "flex", flexDirection: "row", justifyContent: "space-between" }}>
         <section style={{ marginLeft: "100px" }}>
           <h2>Reset Your Password</h2>
-          {/* how do i add closeput 'x' */}
 
           <p>Enter you Compsigh email and we'll send you a link to reset your password.</p>
+
           <form action="">
             <input type="email" name="resetpassword" placeholder="E-Mail" required />
             <br />
 
             <button>Reset Password</button>
-            {/* how do i make this close the popup */}
             <button onClick={() => setDisplay("signIn")}>Cancel</button>
+
           </form>
         </section>
-        <section style={{height:"920px"}}>
-           <img src={SignIn} alt="" style= {{width: "600px", height: "920px"}}/>
+
+        <section style={{height:"700px"}}>
+           <img src={SignOut} alt=""/>
         </section>
+
         </section>
       )}
-      <div>{/* Graphics content for the page */}</div>
+
       <Footer />
     </>
   );
