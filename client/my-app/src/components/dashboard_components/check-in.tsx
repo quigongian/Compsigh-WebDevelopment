4562ffc46111a67c1abfb621f8cef27f711aa9c9
[LauraAurora] <[laura.apenza@gmail.com]>
2022-11-17 15:39:29 -0500

    grid-template, drop-down, submit button

--- client/my-app/src/components/dashboard_components/check-in.tsx
+++ client/my-app/src/components/dashboard_components/check-in.tsx
@@ -7,63 +7,86 @@ import {BiChevronDown} from 'react-icons/bi';
 import "./check-in.css";
 
 export const CheckIn = () => {
+  const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  const month = months[new Date().getMonth()];
+  const day = new Date().getDate();
+  const year = new Date().getFullYear();
+
   return (
     <>
-    <div className="checkin-container"> 
-      <div className="checkin-header" style = {{marginBottom: "0"}}>
-                    {/* Doge -> Users Name */}
-          <h2 style = {{marginLeft: "12%", color: "#FFFFFF", lineHeight: "0%", marginTop: "3%", fontSize: "50px", fontWeight: "600"}}> Hey Doge, how was your day?</h2>
-                    {/*Retrieve current date */}
-          <p style={{marginLeft: "12%", fontWeight: "lighter", color: "#FFFFFF", lineHeight: "35px", fontSize: "25px"}}>November 18, 2022</p>
+      <div className="checkin-container"> 
+        <div className="checkin-header" style = {{marginBottom: "0"}}>
+                      {/* Doge -> Users Name */}
+            <h2 style = {{marginLeft: "20%", color: "#FFFFFF", lineHeight: "0%", marginTop: "3%", fontSize: "45px", fontWeight: "600"}}>
+               Hey [name], how was your day?</h2>
+            <p style={{marginLeft: "20%", fontWeight: "lighter", color: "#FFFFFF", lineHeight: "35px", fontSize: "25px"}}>
+              {month} {day}, {year}</p>
+        </div>
+            <Questions />
+            <Calendar />
+            <Previous />
       </div>
-      <div className="checkin-grid-container">
-          <Questions />
-          <Calendar />
-          <Previous />
-      </div>
-    </div>
     </>
   );
 };
 
 
 const Questions = () => {
-  const [open, setOpen] = useState(false);
-  // const [answer, setAnswer] = useState(null);
-  // useEffect(() => {
-  //  fetch()
-  //   .then((res) => res.json())
-  //   .then((data) => {
-  //     console.log(data);
-  //   }) 
-  // }, [])
+  const [open1, setOpen1] = useState(false);
+  const [open2, setOpen2] = useState(false);
+  const [open3, setOpen3] = useState(false);
+  const [open4, setOpen4] = useState(false);
   return (
   <div className = "questions-container">
     <div className = "questions">
       <p style = {{marginBottom: "0"}}>How productive would you say you were today?</p>
-        <div className = "Selector" onClick = {() => setOpen(!open)}>         
+        <div className = "drop-down" onClick = {(e) => setOpen1(!open1)}>      
           <BiChevronDown style = {{cursor: "pointer", color: "#908484"}}/>
         </div>
-      {/* <ul className = "selector-container" style = {{backgroundColor: "#DDDBDB", width: "80%", cursor: "pointer"}}>
-        {/* Color depth if hover */}
-        {/* <li className = "options">Great</li>
-        <li className = "options">Alright</li>
-        <li className = "options">Bad</li>
-      </ul> */}
+        {open1 && (
+          <div className = "drop-down-content">
+            <div className = "drop-down-content-item"> Great </div>
+            <div className = "drop-down-content-item"> Alright </div>
+            <div className = "drop-down-content-item"> Terrible </div>
+          </div>
+        )}
       <p style = {{marginBottom: "0"}}>Have you completed all your tasks?</p>
-      <div className = "Selector" style = {{}}>
+      <div className = "drop-down" onClick = {(e) => setOpen2(!open2)}> 
         <BiChevronDown style = {{cursor: "pointer", color: "#908484"}}/>
       </div>
-      <p style = {{marginBottom: "0"}}>Are you making any progress?</p>
-      <div className = "Selector">
+      {open2 && (
+          <div className = "drop-down-content">
+            <div className = "drop-down-content-item"> Yes </div>
+            <div className = "drop-down-content-item"> Somewhat </div>
+            <div className = "drop-down-content-item"> No, not at all </div>
+          </div>
+        )}
+      <p style = {{marginBottom: "0"}}>Are you making any progress towards your goals?</p>
+      <div className = "drop-down" onClick = {(e) => setOpen3(!open3)}> 
         <BiChevronDown style = {{cursor: "pointer", color: "#908484"}}/>
       </div>
+      {open3 && (
+          <div className = "drop-down-content">
+            <div className = "drop-down-content-item"> Yes, today I have </div>
+            <div className = "drop-down-content-item"> No, I have not been making progress </div>
+          </div>
+        )}
       <p style = {{marginBottom: "0"}}>Do you foresee any problems in progress?</p>
-      <div className = "Selector">
+      <div className = "drop-down" onClick = {(e) => setOpen4(!open4)}> 
         <BiChevronDown style = {{cursor: "pointer", color: "#908484"}}/>
       </div>
-      <p style = {{marginBottom: "0"}}>What are those problems?</p>
+      {open4 && (
+          <div className = "drop-down-content">
+            <div className = "drop-down-content-item"> Definitely </div>
+            <div className = "drop-down-content-item"> Possibly </div>
+            <div className = "drop-down-content-item"> No </div>
+          </div>
+        )}
+      <p style = {{marginBottom: "0"}}>If so, what are those problems?</p>
     </div>
+      <div className = "button-container" /* onClick={submit}*/ >
+        <button className = "button"> Submit </button>
+      </div>
   </div>
   );
 };
