328e9d30bc2f26de5961336c37879c7af2fdbebe
Dominick DEV <74343440+DominickDiaz@users.noreply.github.com>
2022-11-29 17:45:25 -0500

    Update Dashboard.tsx

--- client/my-app/src/pages/Dashboard.tsx
+++ client/my-app/src/pages/Dashboard.tsx
@@ -1,10 +1,11 @@
 import Sidebar from "../components/dashboard_components/sidebar";
+import Music from "../components/dashboard_components/Music";
 
 export const Dashboard = () => {
-  return (
-    <>
-      <Sidebar />
-      <Music />
-    </>
-  );
+    return (
+            <>
+            <Sidebar />
+            <Music />
+            </>
+            );
 };
