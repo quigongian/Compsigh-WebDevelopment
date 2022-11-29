98bce2cd01bc915f4cb60372dce2dfdecf582376
[LauraAurora] <[laura.apenza@gmail.com]>
2022-11-29 16:48:21 -0500

    Attempted to complete the colors for the Calendar

--- client/my-app/src/components/dashboard_components/CalendarTracker.tsx
+++ client/my-app/src/components/dashboard_components/CalendarTracker.tsx
@@ -3,6 +3,15 @@ import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 import './CalendarTracker.css';
 
+const mark = [
+  {date: '12-02-2022', values: '#499570'},
+  {date: '12-04-2022', values: '#499570'},
+  {date: '12-06-2022', values: '#D1A710'},
+  {date: '12-10-2022', values: '#D1A710'},
+  {date: '12-11-2022', values: '#DA5D3C'},
+  {date: '12-12-2022', values: '#499570'},
+]
+
 const CalendarTracker = () => {
   const [value, onChange] = useState(new Date());
 
@@ -14,3 +23,30 @@ const CalendarTracker = () => {
 }
 
 export default CalendarTracker;
+
+// const CalendarTracker = () => {
+  
+//   setDate = (date: { getDay: () => number; getMonth: () => number; getDate: () => number; }) => {
+//   const dateobj =
+//     mark.find((x) => {
+//       return (
+//         date.getDay() === new Date(x.date).getDay() &&
+//         date.getMonth() === new Date(x.date).getMonth() &&
+//         date.getDate() === new Date(x.date).getDate()
+//       );
+//     });
+//     return dateobj ? dateobj.values : "";
+//   };
+
+//   const [date, setDate] = useState(new Date());
+
+
+//   return (
+//     <Calendar onChange={setDate} value={date}
+//       tileClassName={({ date, view }) => this.setDate(mark)} />
+//   );
+// }
+
+
+
+// export default CalendarTracker;
